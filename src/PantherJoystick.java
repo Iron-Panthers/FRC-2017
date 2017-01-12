b563e027972be86fc7c147ecc812af74c1ec2830
Sarsie <serenanh@gmail.com>
2017-01-11 16:58:29 -0800

    need to finish joystick

---
+++ src/PantherJoystick.java
@@ -0,0 +1,12 @@
+import edu.wpi.first.wpilibj.Joystick;
+
+public class PantherJoystick extends Joystick {
+	
+	double scaling;
+	double xVal;
+	double yVal;
+	
+	public PantherJoystick() {
+		
+	}
+}
