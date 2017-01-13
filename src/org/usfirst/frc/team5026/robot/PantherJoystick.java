04600a6d8a5ccaa9a1b48e0a181ab2c9b6e7bb15
Sc2ad <adamznow@gmail.com>
2017-01-12 20:14:49 -0800

    Class details added

--- src/org/usfirst/frc/team5026/robot/PantherJoystick.java
+++ src/org/usfirst/frc/team5026/robot/PantherJoystick.java
@@ -4,6 +4,17 @@ import org.usfirst.frc.team5026.robot.Constants;
 
 import edu.wpi.first.wpilibj.Joystick;
 
+/* 
+ * @param magnitude is the magnitude of the vector of the joystick from the center. 
+ * @param xy is a X or Y value usually obtained from getX() or getY()
+ * 
+ * The math for the radial deadzone is found at this website: 
+ * http://www.third-helix.com/2013/04/12/doing-thumbstick-dead-zones-right.html
+ * 
+ * One thing to take notice is the (xy / magnitude) or normalized vector also known as the unit vector.
+ * 
+ * */
+
 public class PantherJoystick extends Joystick{
 	
 	int joystickThrottleValue;
