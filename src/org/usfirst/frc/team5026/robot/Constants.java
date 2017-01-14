2a4374170be8ba4b20474f022e21a799e948b9e9
Sarsie <serenanh@gmail.com>
2017-01-13 16:50:07 -0800

    work on joystick

--- src/org/usfirst/frc/team5026/robot/Constants.java
+++ src/org/usfirst/frc/team5026/robot/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
 	
 	public static final int DRIVE_LEFT_MOTOR = 0;
 	public static final int DRIVE_RIGHT_MOTOR = 1;
+	public static final float DEADZONE_RADIUS = 0.1f;
+	public static final float SCALING = 0.5;
 
 }
