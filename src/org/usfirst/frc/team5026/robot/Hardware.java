a828322c00126bcd768bdee42599bf7fadf655bf
Maxim <maximumpwnguin@gmail.com>
2017-01-24 19:17:40 -0800

    Create shifting command

--- src/org/usfirst/frc/team5026/robot/Hardware.java
+++ src/org/usfirst/frc/team5026/robot/Hardware.java
@@ -1,15 +1,18 @@
 package org.usfirst.frc.team5026.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Spark;
 
 public class Hardware {
 	public Spark leftMotor;
 	public Spark rightMotor;
+	
+	public DoubleSolenoid shifter; // pls init pls i duno hao
 
 	public Hardware() {
 		leftMotor = new Spark(RobotMap.DRIVE_MOTOR_LEFT);
 		rightMotor = new Spark(RobotMap.DRIVE_MOTOR_RIGHT);
 		
-
+		// something something new doublesolenoid
 	}
 }
--- src/org/usfirst/frc/team5026/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team5026/robot/subsystems/Drive.java
@@ -1,11 +1,12 @@
 package org.usfirst.frc.team5026.robot.subsystems;
 
-import org.usfirst.frc.team5026.robot.Hardware;
 import org.usfirst.frc.team5026.robot.PantherJoystick;
 import org.usfirst.frc.team5026.robot.Robot;
 import org.usfirst.frc.team5026.robot.RobotMap;
 import org.usfirst.frc.team5026.robot.commands.drive.DriveWithJoystick;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -13,16 +14,25 @@ public class Drive extends Subsystem {
 	private RobotDrive drive;
 	
 	private PantherJoystick joystick;
-	private Hardware hardware;
+	private DoubleSolenoid shifter;
 	
 	public Drive() {
 		joystick = new PantherJoystick(RobotMap.DRIVE_JOYSTICK);
-		hardware = Robot.hardware;
-		drive = new RobotDrive(hardware.leftMotor, hardware.rightMotor);
+		drive = new RobotDrive(Robot.hardware.leftMotor, Robot.hardware.rightMotor);
+		shifter = Robot.hardware.shifter;
 	}
 	
 	public void setLeftRightMotors(double left, double right) {
 		drive.setLeftRightMotorOutputs(left, right);
+	} 
+	
+	public void setGear(GearPosition pos) {
+		switch (pos) {
+		case HIGH:
+			shifter.set(Value.kForward);
+		case LOW:
+			shifter.set(Value.kReverse);
+		}
 	}
 	
 	public void useArcadeDrive(double yAxis, double xAxis) {
