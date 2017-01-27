f0be1ee566e6fb34a34c481be9969518172bff88
Sarsie <serenanh@gmail.com>
2017-01-26 16:59:51 -0800

    Hardware Drive fixes

--- src/org/usfirst/frc/team5026/robot/Hardware.java
+++ src/org/usfirst/frc/team5026/robot/Hardware.java
@@ -1,17 +1,21 @@
 package org.usfirst.frc.team5026.robot;
 
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Hardware {
-	public Talon leftMotor;
-	public Talon rightMotor;
-	public Talon talonMotor;
+	public CANTalon leftMotor_1;
+	public CANTalon leftMotor_2; 
+	public CANTalon leftMotor_3;
+	public CANTalon rightMotor_4;
+	public CANTalon rightMotor_5;
+	public CANTalon rightMotor_6;
 	public Talon climbRightMotor;
 	public Talon climbLeftMotor;
 	public DoubleSolenoid shifter;
@@ -23,8 +27,12 @@ public class Hardware {
 	public DigitalInput gearClampSensor;
 
 	public Hardware() {
-		leftMotor = new Talon(RobotMap.DRIVE_MOTOR_LEFT);
-		rightMotor = new Talon(RobotMap.DRIVE_MOTOR_RIGHT);
+		leftMotor_1 = new CANTalon(RobotMap.DRIVE_MOTOR_LEFT);
+		leftMotor_2 = new CANTalon(RobotMap.DRIVE_MOTOR_LEFT);
+		leftMotor_3 = new CANTalon(RobotMap.DRIVE_MOTOR_LEFT);
+		rightMotor_4 = new CANTalon(RobotMap.DRIVE_MOTOR_RIGHT);
+		rightMotor_5 = new CANTalon(RobotMap.DRIVE_MOTOR_RIGHT);
+		rightMotor_6 = new CANTalon(RobotMap.DRIVE_MOTOR_RIGHT);
 		climbRightMotor = new Talon(RobotMap.CLIMB_MOTOR_RIGHT);
 		climbLeftMotor = new Talon(RobotMap.CLIMB_MOTOR_LEFT);
 		gyro = new ADXRS450_Gyro(Port.kOnboardCS0);
