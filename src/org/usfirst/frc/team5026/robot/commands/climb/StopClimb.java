8000f1d3c485e83185e87e2879a6437c41beb629
Austin Soohoo <austin.soohoo@gmail.com>
2017-01-27 17:23:15 -0800

    change stopclimb indents

--- src/org/usfirst/frc/team5026/robot/commands/climb/StopClimb.java
+++ src/org/usfirst/frc/team5026/robot/commands/climb/StopClimb.java
@@ -6,39 +6,38 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class StopClimb extends Command {
 
-		private Climber climber;
-		
-	    public StopClimb() {
-	        // Use requires() here to declare subsystem dependencies
-	        requires(Robot.climber);
-	        climber = Robot.climber;
-	    }
+	private Climber climber;
+	
+    public StopClimb() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.climber);
+        climber = Robot.climber;
+    }
 
-	    // Called just before this Command runs the first time
-	    protected void initialize() {
-	    	climber.stopClimb();
-	    }
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	climber.stopClimb();
+    }
 
-	    // Called repeatedly when this Command is scheduled to run
-	    protected void execute() {
-	    	climber.stopClimb();
-	    }
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	climber.stopClimb();
+    }
 
-	    // Make this return true when this Command no longer needs to run execute()
-	    protected boolean isFinished() {
-	    	return false;
-	    }
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	return false;
+    }
 
-	    // Called once after isFinished returns true
-	    protected void end() {
-	    	climber.stopClimb();
-	    }
+    // Called once after isFinished returns true
+    protected void end() {
+    	climber.stopClimb();
+    }
 
-	    // Called when another command which requires one or more of the same
-	    // subsystems is scheduled to run
-	    protected void interrupted() {
-	    		end();
-	    }
-	}
-
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    		end();
+    }
 }
+
