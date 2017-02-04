1cf25f3008ea21bf78240255116ec5048367928c
Austin Soohoo <austin.soohoo@gmail.com>
2017-02-04 15:23:36 -0800

    Added todos

--- src/org/usfirst/frc/team5026/robot/Constants.java
+++ src/org/usfirst/frc/team5026/robot/Constants.java
@@ -14,8 +14,8 @@ public class Constants {
 	
 	//Climber Constants
 	public static final double CLIMBER_RAPPEL_SPEED = -0.2;	//quality of unloader's life
-	public static final double CLIMBER_SLOW_SPEED = 0.1;	
-	public static final double CLIMBER_LATCH_SPEED = 0.3;	//Tested multiple times with driver, comfortable speed
+	public static final double CLIMBER_SLOW_SPEED = 0.1;	//original: 0.3, changed for testing thresholds
+	public static final double CLIMBER_LATCH_SPEED = 0.3;	//Tested multiple times with driver, comfortable speed NEED INCREASE
 	public static final double CLIMBER_WRAP_SPEED = 0.71;	//Soft average, tested once, will need to be optimized a bit further
 	public static final double CLIMBER_FLOOR_SPEED = 1.0;	//Floors motors
 	public static final double CLIMBER_CURVE_SWAP = 0.0;	//The value in which the joystick switches from linear to quadratic
