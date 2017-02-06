aed17254c5d1dec756ef50946e1d3e77ddcc6ded
Sc2ad <adamznow@gmail.com>
2017-02-06 12:10:14 -0800

    Fixed Gearsensor mappings

--- src/org/usfirst/frc/team5026/util/Hardware.java
+++ src/org/usfirst/frc/team5026/util/Hardware.java
@@ -50,5 +50,6 @@ public class Hardware {
 		climberLeftMotor = new Talon(RobotMap.CLIMBER_MOTOR_LEFT);
 		gyro = new ADXRS450_Gyro(Port.kOnboardCS0);
 		gearClampPiston = new DoubleSolenoid(1, RobotMap.gearPistonForward, RobotMap.gearPistonReverse);
+		gearClampSensor = new DigitalInput(RobotMap.gearClampSensor);
 	}
 }
