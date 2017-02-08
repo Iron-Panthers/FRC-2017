5e272c092af3ea87c794625151931ada5e669f36
Isabel Dominik <icd107@live.com>
2017-02-07 16:37:28 -0800

    formatting

--- src/org/usfirst/frc/team5026/robot/subsystems/GearClamp.java
+++ src/org/usfirst/frc/team5026/robot/subsystems/GearClamp.java
@@ -8,28 +8,28 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  
 public class GearClamp extends Subsystem { 
  
-  private Hardware hardware; 
-   
-  public GearClamp(){ 
-    hardware = Robot.hardware; 
-  } 
-  @Override 
-  protected void initDefaultCommand() { 
-    // TODO Auto-generated method stub 
-     
-  } 
- 
-  public void clampOnGear(){ 
-    hardware.gearClampPiston.set(Value.kForward);
-    //Clamps
-  } 
-  public void lowerClamp(){ 
-    hardware.gearClampPiston.set(Value.kReverse); 
-    //Unclamps
-  } 
-  public boolean hasGear(){ 
-    return Robot.hardware.gearClampSensor.get();
-    //Banner sensor on the gear clamp that detects whether there's a gear in it
-  } 
+	  private Hardware hardware; 
+	   
+	  public GearClamp(){ 
+		  hardware = Robot.hardware; 
+	  } 
+	  @Override 
+	  protected void initDefaultCommand() { 
+		  // TODO Auto-generated method stub 
+	  
+	  } 
+	 
+	  public void clampOnGear(){ 
+		  hardware.gearClampPiston.set(Value.kForward);
+		  //Clamps
+	  } 
+	  public void lowerClamp(){ 
+		  hardware.gearClampPiston.set(Value.kReverse); 
+		  //Unclamps
+	  } 
+	  public boolean hasGear(){ 
+		  return Robot.hardware.gearClampSensor.get();
+		  //Banner sensor on the gear clamp that detects whether there's a gear in it
+	  } 
    
 } 
