95ce57784f9f029ebf16ae891b9ea1e8f16cfc6f
Sc2ad <adamznow@gmail.com>
2017-02-11 11:04:46 -0800

    Deadzone X Fixes, Updated Joystick displays

--- src/org/usfirst/frc/team5026/robot/OI.java
+++ src/org/usfirst/frc/team5026/robot/OI.java
@@ -1,10 +1,10 @@
 package org.usfirst.frc.team5026.robot;
 
-import org.usfirst.frc.team5026.robot.commands.drive.DriveSwapForwards;
 import org.usfirst.frc.team5026.robot.commands.climber.ClimberUpWithJoystick;
+import org.usfirst.frc.team5026.robot.commands.drive.DriveDrivebaseForTime;
 import org.usfirst.frc.team5026.robot.commands.drive.DriveShiftGear;
+import org.usfirst.frc.team5026.robot.commands.drive.DriveSwapForwards;
 import org.usfirst.frc.team5026.robot.commands.gear.GearClampCommand;
-import org.usfirst.frc.team5026.robot.commands.gear.GearClampCommandGroup;
 import org.usfirst.frc.team5026.robot.commands.gear.GearUnClampCommand;
 import org.usfirst.frc.team5026.util.PantherJoystick;
 
