67e3bfbd09af96f69f3e50852de273982ce0f49e
Austin Soohoo <austin.soohoo@gmail.com>
2017-02-12 11:18:31 -0800

    Removed backward climbing

--- src/org/usfirst/frc/team5026/robot/commands/climber/ClimberSequence.java
+++ src/org/usfirst/frc/team5026/robot/commands/climber/ClimberSequence.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class ClimberSequence extends CommandGroup {
 	//runs climber motors and with a built in time
     public ClimberSequence() {
-        addSequential(new WaitCommand(1));
         addSequential(new ClimberUpClimb()); //hasResistance()
         addSequential(new ClimberStop());
     }
