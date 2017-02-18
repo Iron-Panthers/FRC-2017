1cefd985d2514bcd9512c1b8bfe9036dde06a2ae
Sc2ad <adamznow@gmail.com>
2017-02-18 12:11:47 -0800

    Compelted Intake

--- src/org/usfirst/frc/team5026/robot/RobotMap.java
+++ src/org/usfirst/frc/team5026/robot/RobotMap.java
@@ -7,22 +7,17 @@ package org.usfirst.frc.team5026.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
-	
+	// CLIMBER MOTORS
 	public static final int CLIMBER_MOTOR_RIGHT = 0;
 	public static final int CLIMBER_MOTOR_LEFT = 1;
 
 	public static final int CLIMBER_PDPMOTOR_RIGHT = 14;
 	public static final int CLIMBER_PDPMOTOR_LEFT = 15;
 	
+	// INTAKE MOTORS
+	public static final int INTAKE_MOTOR = 0;
+	
+	// DRIVE MOTORS
 	public static final int DRIVE_MOTOR_LEFT_1 = 4;
 	public static final int DRIVE_MOTOR_LEFT_2 = 5;
 	public static final int DRIVE_MOTOR_LEFT_3 = 6;
@@ -30,11 +25,13 @@ public class RobotMap {
 	public static final int DRIVE_MOTOR_RIGHT_2 = 2;
 	public static final int DRIVE_MOTOR_RIGHT_3 = 3;
 	
-	public static final int DRIVE_JOYSTICK = 0;
-	public static final int BUTTON_JOYSTICK = 1;
-	
+	// SOLENOIDS
 	public static final int SOLENOID_SHIFTER_FORWARD = 0;
 	public static final int SOLENOID_SHIFTER_REVERSE = 1;
+	
+	// JOYSTICK BUTTONS
+	public static final int DRIVE_JOYSTICK = 0;
+	public static final int BUTTON_JOYSTICK = 1;
 	
 	public static final int BOARD_BUTTON_1 = 1;
 	public static final int BOARD_BUTTON_2 = 2;
