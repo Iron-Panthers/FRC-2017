af34dc7d1f1a0e94a6143983802b3fea88556e90
Sc2ad <adamznow@gmail.com>
2017-03-15 22:54:17 -0700

    Fixed duplicate constants

--- src/org/usfirst/frc/team5026/util/Constants.java
+++ src/org/usfirst/frc/team5026/util/Constants.java
@@ -48,21 +48,6 @@ public class Constants {
 	public static final double WHEEL_CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
 	public static final int ENCODER_TICKS_PER_ROTATION = 1024;
 	
-	//Auto COnstants
-	public static final String DRIVE_DISTANCE_RAMP_SMD_NAME = "DriveRampDistance(inches)"; 
-	public static final String DRIVE_TURNXDEGREES_NAME = "DriveTurnXDegrees(degrees)"; 
-	public static final double STRAIGHT_DRIVE_SPEED = 0.5;
-	public static final double GEAR_RATIO = 5.36;
-	
-	// Gyro Constants
-	public static final double PERCENTAGE_FOR_ERROR = 0.05; 
-	public static final double AUTO_TURN_SPEED = 0.1;
-	
-	// Encoder constants
-	public static final double WHEEL_DIAMETER = 4;	//in inches
-	public static final double WHEEL_CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
-	public static final int ENCODER_TICKS_PER_ROTATION = 1024;
-	
 	// Auto Constants
 	public static final double STRAIGHT_DRIVE_SPEED = 0.5;
 	public static final double GEAR_RATIO = 5.36;
