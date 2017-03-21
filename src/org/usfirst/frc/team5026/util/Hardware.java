dbb70ca082b38b590482e26e5103ae9ba079b8d0
Isabel Dominik <icd107@live.com>
2017-03-21 16:20:53 -0700

    Lowered climb speed to not burn out motors, changed climb inversions
    Max changed climb motors, so wiring/inversions changed on comp bot

--- src/org/usfirst/frc/team5026/util/Hardware.java
+++ src/org/usfirst/frc/team5026/util/Hardware.java
@@ -29,8 +29,8 @@ public class Hardware {
 	public Talon climberRightMotor;
 	public Talon climberLeftMotor;
 	
-	public boolean climberLeftInverted = true;
-	public boolean climberRightInverted = true; //comp bot: true, practice bot: false
+	public boolean climberLeftInverted = false;
+	public boolean climberRightInverted = false; //comp bot: true, practice bot: false
 	
 	public Talon intake;
 	
