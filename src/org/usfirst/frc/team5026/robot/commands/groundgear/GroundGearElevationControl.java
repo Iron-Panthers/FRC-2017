e839d3ae75a9448dc5f03a4666dd5b2ba2f86d9e
Sc2ad <adamznow@gmail.com>
2017-09-10 18:09:09 -0700

    Removed GearClampControl, NEED TO FIX

--- src/org/usfirst/frc/team5026/robot/commands/groundgear/GroundGearElevationControl.java
+++ src/org/usfirst/frc/team5026/robot/commands/groundgear/GroundGearElevationControl.java
@@ -12,8 +12,6 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class GroundGearElevationControl extends Command {
 	GroundGearElevationState target;
-	int count;
-	int maxCount;
 
     public GroundGearElevationControl(GroundGearElevationState target) {
         requires(Robot.groundgear);
@@ -22,25 +20,21 @@ public class GroundGearElevationControl extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	maxCount = Constants.GROUND_GEAR_STABILISATION_MAX_COUNT;
-    	count = 0;
-    	Robot.groundgear.setup();
+//    	Robot.groundgear.setup();
+    	Robot.groundgear.getElevationMotor().enable();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	boolean allowed;
-    	if (target == GroundGearElevationState.Legal) {
-    		allowed = true;
-    	} else {
-    		allowed = CanGearClampsMove.checkMovement(Robot.groundgear, Robot.gearclamp);
-    	}
+    	boolean allowed = true;
+//    	if (target == GroundGearElevationState.Legal) {
+//    		allowed = true;
+//    	} else {
+//    		allowed = CanGearClampsMove.checkMovement(Robot.groundgear, Robot.gearclamp);
+//    	}
     	if (allowed) {
     		// Safe error checking, checks gear movement before continuing
     		Robot.groundgear.travelToState(target);
-//	    	if(Math.abs(Robot.hardware.groundGearLift.getClosedLoopError()) < Constants.GROUND_GEAR_TICK_TOLERANCE) {
-//	        	count++;
-//	        }
     	}
     	else {
     		// If there is illegal movement, should not be done! Cancels the command (calls interrupted)
@@ -50,13 +44,11 @@ public class GroundGearElevationControl extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return count >= maxCount;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.groundgear.stopLift();
-    	Robot.groundgear.setElevationState(target);
     }
 
     // Called when another command which requires one or more of the same
@@ -65,6 +57,6 @@ public class GroundGearElevationControl extends Command {
     	// Perhaps move back to previous state?
     	// For now, just stop
     	// Notable issue: If trying to intake before fully in correct state, state movement will cancel!
-    	Robot.groundgear.stopLift();
+//    	Robot.groundgear.stopLift();
     }
 }
