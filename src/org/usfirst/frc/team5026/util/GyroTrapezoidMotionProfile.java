f3ee89d085c125bf9009bf651b099fed9396c636
Sc2ad <adamznow@gmail.com>
2017-09-30 00:49:54 -0700

    Added Trapezoid profile control, when there are no integrated srx sensor

---
+++ src/org/usfirst/frc/team5026/util/GyroTrapezoidMotionProfile.java
@@ -0,0 +1,62 @@
+package org.usfirst.frc.team5026.util;
+
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+public class GyroTrapezoidMotionProfile {
+	
+	// Variables:
+	/*
+	 * Gyro
+	 * ACCEL
+	 * CRUISE_VEL
+	 * P
+	 * I
+	 * D
+	 * F
+	 */
+	Gyro gyro;
+	double ACCELERATION;
+	double VELOCITY;
+	double[] pidf;
+	TrapezoidMotionProfilePath path;
+	
+	double target;
+	
+	
+	private double lastPosError;
+	private double totalPosError;
+	
+	public GyroTrapezoidMotionProfile(Gyro gyro, double a, double v, double... pidf) {
+		this.gyro = gyro;
+		ACCELERATION = a;
+		VELOCITY = v;
+		if (pidf.length == 4) {
+			this.pidf = pidf;
+		}
+		path = new TrapezoidMotionProfilePath(v,a);
+	}
+	public void set(double target) {
+		this.target = target;
+		path.set(target);
+		path.calculateCurve();
+		lastPosError = 0; // default
+		totalPosError = 0;
+	}
+	public double getOut(double time) {
+		double realPos = gyro.getAngle(); // This is deg
+		double realVel = gyro.getRate(); // This is deg/s
+		double posHat = path.getPos(time);
+		double velHat = path.getVel(time);
+		double currentPosError = realPos - posHat;
+		double currentVelError = realVel - velHat;
+		double p = pidf[0];
+		double i = pidf[1];
+		double d = pidf[2];
+		double f = pidf[3];
+		
+		double out = p * (currentPosError) + i * (totalPosError) + d * (currentPosError - lastPosError) + f * (currentVelError);
+		
+		lastPosError = currentPosError;
+		return out;
+	}
+}
